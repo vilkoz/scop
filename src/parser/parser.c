01f649e483c0e6351713ee9df08a44f9d2cb4cce
Vitalii Rybalko <vilko.a@gmail.com>
2018-03-19 00:32:03 +0200

    starting parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: vrybalko <vrybalko@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 00:52:12 by vrybalko          #+#    #+#             */
-/*   Updated: 2018/03/18 23:53:41 by vrybalko         ###   ########.fr       */
+/*   Updated: 2018/03/19 00:29:34 by vrybalko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,23 +36,74 @@ char *read_file_to_string(char *filename)
 	return (buffer);
 }
 
-t_list				*v_parser(char **lines, int *i)
+static char		*g_names[5] = {
+	"v ",
+	"vt ",
+	"vn ",
+	"f ",
+	NULL
+};
+
+static t_list		(*g_functions[5])(char **, int *, char *) = {
+	face_elment_parser,
+	face_elment_parser,
+	face_elment_parser,
+	face_parser,
+	NULL
+};
+
+t_list				*face_parser(char **lines, int *i, char *prefix)
+{
+	// TODO: FORMAT LINE that supports 15/34/234 //234 //234
+	t_list		*vertices;
+	t_vertex	v;
+	char		format_line[20];
+
+	ft_bzero((void*)&(format_line[0]), 20);
+	ft_strncat(&(format_line[0]), prefix, 20);
+	ft_strncat(&(format_line[0]), "%9lf %9lf %9lf", 20);
+	vertices = NULL;
+	while (lines[++(*i)] && !ft_strncmp(lines[(*i)], prefix, ft_strlen(prefix)))
+	{
+		sscanf(lines[*i], &(format_line[0]), &(v.x), &(v.y), &(v.z));
+		ft_lstadd(&vertices, ft_lstnew((void*)&v, sizeof(t_vertex)));
+	}
+	return (vertices);
+}
+
+t_list				*face_elment_parser(char **lines, int *i, char *prefix)
 {
 	t_list		*vertices;
+	t_vertex	v;
+	char		format_line[20];
 
+	ft_bzero((void*)&(format_line[0]), 20);
+	ft_strncat(&(format_line[0]), prefix, 20);
+	ft_strncat(&(format_line[0]), "%9lf %9lf %9lf", 20);
 	vertices = NULL;
-	while (lines[++(*i)] && ft_strncmp(lines[(*i)], "v ", 2))
+	while (lines[++(*i)] && !ft_strncmp(lines[(*i)], prefix, ft_strlen(prefix)))
 	{
+		sscanf(lines[*i], &(format_line[0]), &(v.x), &(v.y), &(v.z));
+		ft_lstadd(&vertices, ft_lstnew((void*)&v, sizeof(t_vertex)));
 	}
+	return (vertices);
 }
 
 t_parsed_object		*obj_parser(char *file_contents)
 {
 	char		**lines;
 	int			i;
+	int			j;
 
 	lines = ft_strsplit(file_contents, '\n');
-	i = -1;
+	i = 0;
+	while (lines[i])
+	{
+		j = -1;
+		while (g_names[++j])
+			if (!ft_strncmp(lines[i], g_names[j], ft_strlen(g_names[j])))
+			// TODO: pass pointer to required vertex array
+	}
 }
 
 t_parsed_object		*obj_file_parser(char *filename)
